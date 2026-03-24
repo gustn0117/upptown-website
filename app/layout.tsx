403689554f1b60efaedf01eabb7dbd8f5457be4f
심현수 <hyunsu@simhyeonsuui-MacBookPro.local>
2026-03-24 20:52:15 +0900

    Initial commit: UPPTOWN website

---
+++ app/layout.tsx
@@ -0,0 +1,4 @@
+export const metadata = { title: 'UPPTOWN - Local Entertainment' };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <html lang="ko"><body>{children}</body></html>;
+}
